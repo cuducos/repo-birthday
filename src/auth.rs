90a18d5050e80b0348684a698839822cb9a81622
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2025-05-16 08:31:05 -0400

    Uses JSON on token exhange

--- src/auth.rs
+++ src/auth.rs
@@ -1,8 +1,8 @@
 use crate::envvar;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use lazy_static::lazy_static;
 use reqwest::Client;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 const USER_AGENT: &str = "github.com/cuducos/repo-birthday";
 
@@ -11,36 +11,50 @@ lazy_static! {
     static ref GITHUB_APP_SECRET: String = envvar::get("GITHUB_APP_SECRET").unwrap();
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct UserInfo {
     login: String,
 }
 
-// #[derive(Deserialize, Debug)]
-// struct AccessToken {
-//     access_token: String,
-// }
+#[derive(Serialize)]
+struct ExchangeToken {
+    code: String,
+    client_id: String,
+    client_secret: String,
+}
+
+#[derive(Deserialize)]
+struct AccessToken {
+    access_token: String,
+}
 
 pub fn github_client_id() -> Result<String> {
     envvar::get("GITHUB_APP_CLIENT_ID")
 }
 
 pub async fn token_for(client: &Client, code: &str) -> anyhow::Result<String> {
-    let params = [
-        ("code", code.to_string()),
-        ("client_id", github_client_id()?),
-        ("client_secret", GITHUB_APP_SECRET.to_string()),
-    ];
+    let params = ExchangeToken {
+        code: code.to_string(),
+        client_id: github_client_id()?,
+        client_secret: GITHUB_APP_SECRET.to_string(),
+    };
     let res = client
         .post("https://github.com/login/oauth/access_token")
         .header("Accept", "application/json")
         .header("User-Agent", USER_AGENT)
-        .form(&params)
+        .json(&params)
         .send()
         .await?;
-    let body = res.text().await?;
-    println!("{}", &body);
-    Ok("test".to_string())
+    if !res.status().is_success() {
+        println!(
+            "Error getting access token ({:?}): {:?}",
+            res.status(),
+            res.text().await?
+        );
+        return Err(anyhow!("Error getting access token"));
+    }
+    let data = res.json::<AccessToken>().await?;
+    Ok(data.access_token)
 }
 
 pub async fn username_for(client: &Client, token: &str) -> anyhow::Result<String> {
@@ -50,8 +64,15 @@ pub async fn username_for(client: &Client, token: &str) -> anyhow::Result<String
         .header("Accept", "application/json")
         .header("User-Agent", USER_AGENT)
         .send()
-        .await?
-        .json::<UserInfo>()
         .await?;
-    Ok(res.login)
+    if !res.status().is_success() {
+        println!(
+            "Error getting username ({:?}): {:?}",
+            res.status(),
+            res.text().await?
+        );
+        return Err(anyhow!("Error getting username"));
+    }
+    let data = res.json::<UserInfo>().await?;
+    Ok(data.login)
 }
